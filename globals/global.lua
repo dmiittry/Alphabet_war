4780a86272656cafac65c817421a25c4e3895dc2
dmiittry <dmitry1993aa@gmail.com>
2023-12-13 04:05:41 +0900

    dead_menu

--- globals/global.lua
+++ globals/global.lua
@@ -5,16 +5,31 @@ GREEN_COLOR = vmath.vector4(0,1,0,1)
 BLUE_COLOR = vmath.vector4(0,0,1,1)
 GREY_COLOR = vmath.vector4(0.4,0.4,0.4,1)
 
-SPEED_WORD = 9
-TIME_SPAWN_WORD = 0.8
+SPEED_WORD = 9 -- 4 min / 9 max
+TIME_SPAWN_WORD = 0.8 -- 0.3 min / 0.8 max
+
+KILL_ALL = false
 TOUCH_WORD = ""
 GAME_MODE = "endless"
+ALL_WORD = 0
+SCORE = 0
+PLAY_TIME = 0
 
 WORD_LIST = {}
 STANDART_LIST = { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" }
 
 POSITION_WORD = {}
 POSITION_LIST = {20,40,60,80,100,120,140,160,180,200,220,240,260,280,300,320,340,360,380,400,420,440,460,480,500,520,540,560,580,600,620,640,660,680,700}
+
+LIFE = 0
+
+function set_life(value)
+	LIFE = LIFE - value
+	if LIFE <= 0 then
+		msg.post("menu:/go#menu", "dead")
+	end
+	msg.post("main:/go#guigame", "set_life", {value = LIFE})
+end
 
 function new_list_word()
 	for key,value in pairs(STANDART_LIST) do 
--- globals/text.lua
+++ globals/text.lua
@@ -2,11 +2,21 @@ LANG_TEXT = {}
 
 RUS_TEXT = {
 	score = "счёт: ",
-	menu = "меню"
+	menu = "||",
+	exit = "Сдаться!",
+	continue = "Продолжить за просмотр рекламы",
+	dead = "Вы проиграли!",
+	play_time = "Время игры: ",
+	word = "Общее кол-во букв: ",
 }
 ENG_TEXT = {
 	score = "score: ",
-	menu = "menu"
+	menu = "||",
+	exit = "Surrender!",
+	continue = "Continue for adverts",
+	dead = "You lose!",
+	play_time = "Play time: ",
+	word = "Total letters: ",
 }
 
 function change_lang(lang)
